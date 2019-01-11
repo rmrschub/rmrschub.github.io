e852de96244fdc4fd23ff4ba48c85c1d6ac186ee
René Schubotz <resc01@Administrators-MacBook-Pro.local>
2019-01-11 11:43:26 +0100

    only resize imgs

--- test.js
+++ test.js
@@ -68,13 +68,13 @@ function preprocess(img)
     //convert the image data to a tensor 
     let tensor = tf.fromPixels(img, 1)
     //resize to 50 X 50
-    const resized = tf.image.resizeBilinear(tensor, [300, 300]).toFloat()
+    //const resized = tf.image.resizeBilinear(tensor, [300, 300]).toFloat()
     // Normalize the image 
     //const offset = tf.scalar(255.0);
     //const normalized = tf.scalar(1.0).sub(resized.div(offset));
     //We add a dimension to get a batch shape 
-    const batched = resized.expandDims(0)
-    return tensor
+    const batched = tensor.expandDims(0)
+    return batched
 
 }
 /*
